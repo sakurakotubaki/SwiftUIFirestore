9bc795bea84d2ce4898f94c5251c169af44ca259
sakurakotubaki <gitonly543@gmail.com>
2023-11-12 15:05:03 +0900

    Initial Commit

---
+++ FirebaseTutorial/FirebaseTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutorialApp.swift
+//  FirebaseTutorial
+//
+//  Created by 橋本純一 on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
