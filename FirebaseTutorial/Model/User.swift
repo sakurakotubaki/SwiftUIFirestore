aa5d5934be5f640422f912ef7dcbe96e9c4ed841
sakurakotubaki <gitonly543@gmail.com>
2023-11-12 16:13:31 +0900

    first commit

---
+++ FirebaseTutorial/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  FirebaseTutorial
+//
+//  Created by 橋本純一 on 2023/11/12.
+//
+
+import Foundation
---
+++ FirebaseTutorial/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  FirebaseTutorial
+//
+//  Created by 橋本純一 on 2023/11/12.
+//
+
+import Foundation
